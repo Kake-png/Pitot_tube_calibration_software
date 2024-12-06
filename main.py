2c06a76e714ac7dde5a1317549de84d8bb4026b1
Kake-png <kmt.shigeshige.progra@gmail.com>
2024-12-06 16:35:47 +0900

    コード整理、値を表示する部分の不具合修正

--- main.py
+++ main.py
@@ -1,50 +1,55 @@
 # ライブラリのインポート
-import tkinter as tk
-from tkinter import filedialog
-import os
-import pandas as pd
-import chardet
-from decimal import Decimal
+import tkinter as tk  #gui
+from tkinter import filedialog   #ファイルを読み込んでくる関数、コード簡素化のために別でimport
+import os   #作業スペースの処理
+import pandas as pd   #数値処理ライブラリ
+import chardet  #文字コード確認用
+from decimal import Decimal   #floatの計算の桁溢れ対策
 
-df = None
+df = None   #グローバルにするために宣言
+def range_rewrite(time_start,time_end):  # 範囲のEntryとLabelに最大・最小値を入れる
+    range_start.delete( 0, tk.END )
+    range_start.insert(0,time_start)
+    range_finish.delete( 0, tk.END )
+    range_finish.insert(0,time_end)
+    time_min = tk.Label(root,text=f"min:{time_start}")
+    time_min.grid(row=4,column=1)
+    time_max = tk.Label(root,text=f"max:{time_end}")
+    time_max.grid(row=4,column=3)
+    pass
 
-#ボタンのコマンド達
+def timeEntry_rewrite(time_origtext,time_newtext): #今の時間間隔と次の時間間隔の書き換え
+    time_orig.delete( 0, tk.END )
+    time_orig.insert(0,time_origtext)
+    time_new.delete( 0, tk.END )
+    time_new.insert(0,time_newtext)
+    pass
+
+#ボタンのコマンド達----------------------------------------------------------------------------------
 def file_select(filepathbox):
     file_path = filedialog.askopenfilename(filetypes=[("all files", "*.txt")])
     filepathbox.delete(0, tk.END)
     filepathbox.insert(tk.END, file_path)
 
-def read_txt(filepathbox,time_orig,time_new, range_start, range_finish,time_max,time_min):
-    time_orig.delete( 0, tk.END )
-    time_orig.insert(0,"Nan")
-    time_new.delete( 0, tk.END )
-    time_new.insert(0,"")
+def read_txt(filepathbox):
+    timeEntry_rewrite("Nan","")
     
     file_path = filepathbox.get()
     editing = file_path.split("/")
     filename = editing[-1]
     directiry_path = file_path.replace(filename,"")
-    print(directiry_path)
+    #print(directiry_path)
     os.chdir(directiry_path)
 
-    with open(filename, 'rb') as f:
+    with open(filename, 'rb') as f:  #文字コードを確認するために一行だけ取り出す
         c = f.read()
-        result = chardet.detect(c)
+        result = chardet.detect(c) #文字コード判定
         #print(result)
-
-    with open(filename, encoding=result["encoding"]) as file:
+    with open(filename, encoding=result["encoding"]) as file: #ファイルを読んで処理
         contents = file.readlines()
-        line_num = 0
-        deletelen = []
         for line in contents:
             line.replace(" ","").replace("\n","").replace("¥n","").replace(":",",")
-            if len(line) == 0:
-                deletelen.insert(line_num)
-            line_num += 1
-        for line in deletelen:
-            del contents[line]
-
-    with open("temporary.csv",mode="w")as file:
+    with open("temporary.csv",mode="w")as file: #一旦csvファイルに読み込み、不要な行も同時に削除　　もっといい処理方法がある気は、する...
         for line in contents:
             if line == "\n":
                 pass
@@ -54,33 +59,23 @@ def read_txt(filepathbox,time_orig,time_new, range_start, range_finish,time_max,
     #これ以降pandasに入れてデータ表にして処理
     global df
     df = pd.read_csv("temporary.csv")
-    os.remove("temporary.csv")
+    os.remove("temporary.csv") #いらなくなったcsvファイルを廃棄
 
     #print(df.columns.values)
     for column in df.columns.values:   #すでにtimeの項目があった場合Entryに代入
         if column == "time":
             time_range = float(df.at[1,"time"]) - float(df.at[0,"time"])
             #print(time_range)
-            time_orig.delete( 0, tk.END )
-            time_orig.insert(0,time_range)
-
+            timeEntry_rewrite(time_range,"")
             time_start = float(df.at[0,"time"])
             time_end   = float(df.at[len(df.index)-1,"time"])
             #print(f"{time_start} , {time_end}")
-            range_start.delete( 0, tk.END )
-            range_start.insert(0,time_start)
-            range_finish.delete( 0, tk.END )
-            range_finish.insert(0,time_end)
-            time_min = tk.Label(root,text=f"min:{time_start}")
-            time_min.grid(row=4,column=1)
-            time_max = tk.Label(root,text=f"max:{time_end}")
-            time_max.grid(row=4,column=3)
+            range_rewrite(time_start,time_end)
 
-
-def time_refresh(time_orig,time_new,range_start,range_finish,time_max,time_min):  #時間の更新ボタンが押されたとき  Nanと数字以外だった場合のtry_except書かないと
+def time_refresh(time_orig,time_new):  #時間の更新ボタンが押されたとき  Nanと数字以外だった場合のtry_except書かないと
     global df
     if time_orig.get() != "Nan" and time_new.get() != "Nan":  #すでにtimeが指定存在した場合
-        tk.messagebox.showinfo('メッセージ', "処理中です1")
+        tk.messagebox.showinfo('メッセージ', "処理中です")
         #df.drop('time', axis=1)
         del df["time"]
         newtime_list = []
@@ -90,27 +85,17 @@ def time_refresh(time_orig,time_new,range_start,range_finish,time_max,time_min):
             insert_time += Decimal(time_new.get())#そのままfloatとかを足すと桁溢れする
         newtimedf = pd.DataFrame(data=newtime_list, columns=["time"])
         df = pd.concat([newtimedf,df], axis=1)
-        print(df.columns.values)
-        time_orig.delete( 0, tk.END )
-        time_orig.insert(0,time_new.get())
-        time_new.delete( 0, tk.END )
-        time_new.insert(0,"Nan")
+        #print(df.columns.values)
+        timeEntry_rewrite(time_new.get(),"Nan")
 
         time_start = float(df.at[0,"time"])
         time_end   = float(df.at[len(df.index)-1,"time"])
         #print(f"{time_start} , {time_end}")
-        range_start.delete( 0, tk.END )
-        range_start.insert(0,time_start)
-        range_finish.delete( 0, tk.END )
-        range_finish.insert(0,time_end)
-        time_min = tk.Label(root,text=f"min:{time_start}")
-        time_min.grid(row=4,column=1)
-        time_max = tk.Label(root,text=f"max:{time_end}")
-        time_max.grid(row=4,column=3)
-        tk.messagebox.showinfo('メッセージ', "値を更新しました1")
+        range_rewrite(time_start,time_end)
+        tk.messagebox.showinfo('メッセージ', "値を更新しました")
 
     elif time_orig.get() == "Nan" and time_new.get() != "Nan":  #timeが存在しなかった場合
-        tk.messagebox.showinfo('メッセージ', "処理中です2")
+        tk.messagebox.showinfo('メッセージ', "処理中です")
         newtime_list = []
         insert_time = 0
         #print(len(df.index))
@@ -119,28 +104,17 @@ def time_refresh(time_orig,time_new,range_start,range_finish,time_max,time_min):
             insert_time += Decimal(time_new.get())#そのままfloatとかを足すと桁溢れする
         newtimedf = pd.DataFrame(data=newtime_list, columns=["time"])
         df = pd.concat([newtimedf,df],axis=1)
-        print(df.columns.values)
-        time_orig.delete( 0, tk.END )
-        time_orig.insert(0,time_new.get())
-        time_new.delete( 0, tk.END )
-        time_new.insert(0,"Nan")
+        #print(df.columns.values)
+        timeEntry_rewrite(time_new.get(),"Nan")
         time_start = float(df.at[0,"time"])
         time_end   = float(df.at[len(df.index)-1,"time"])
-        #print(f"{time_start} , {time_end}")
-        range_start.delete( 0, tk.END )
-        range_start.insert(0,time_start)
-        range_finish.delete( 0, tk.END )
-        range_finish.insert(0,time_end)
-        time_min = tk.Label(root,text=f"min:{time_start}")
-        time_min.grid(row=4,column=1)
-        time_max = tk.Label(root,text=f"max:{time_end}")
-        time_max.grid(row=4,column=3)
-        tk.messagebox.showinfo('メッセージ', "値を更新しました2")
+        range_rewrite(time_start,time_end)
+        tk.messagebox.showinfo('メッセージ', "値を更新しました")
 
     else:
         tk.messagebox.showinfo('メッセージ', "正しい値を入力してください")
 
-    print(df["time"])
+    #print(df["time"])
     df.to_csv("new.csv")
 
 def start_calc(range_start,range_finish,average_index,str_index,var_index):
@@ -155,23 +129,18 @@ def start_calc(range_start,range_finish,average_index,str_index,var_index):
             #valueindex = tk.Label(root,df_for_calc.columns.values)
             
             mean = df_for_calc["ピトー管係数"].mean()
-            #print(f"平均 : {mean}")
             str_ = df_for_calc["ピトー管係数"].std(ddof=0)
-            #print(f"標準偏差 : {std}")
             var = df_for_calc["ピトー管係数"].var()
+            #print(f"平均 : {mean}")
+            #print(f"標準偏差 : {std}")
             #print(f"分散 : {var}")
-            average_index = tk.Label(root, text=f"平均:{mean}")
-            average_index.grid(row=5,column=1)
+            average_index["text"] = f"平均:{mean}"
+            str_index["text"] = f"標準偏差:{str_}"
+            var_index["text"] = f"分散:{var}"
 
-            str_index = tk.Label(root, text=f"標準偏差:{str_}")
-            str_index.grid(row=6,column=1)
-
-            var_index = tk.Label(root, text=f"分散:{var}")
-            var_index.grid(row=7,column=1)
             return None
     tk.messagebox.showinfo('メッセージ', "データにtimeを設定してください")
-
-
+#----------------------------------------------------------------------------------------------------------------------------
 
 # tkオブジェクトの作成
 root = tk.Tk()
@@ -185,7 +154,7 @@ filepathbox.grid(row=0, column=0, columnspan=2, sticky=tk.W+tk.E)
 filesetbutton = tk.Button(root, text="ファイル選択", command=lambda:file_select(filepathbox))
 filesetbutton.grid(row=0, column=2)
 
-import_btn = tk.Button(root, text="読み込み", command = lambda:read_txt(filepathbox,time_orig,time_new,range_start,range_finish,time_max,time_min))
+import_btn = tk.Button(root, text="読み込み", command = lambda:read_txt(filepathbox))
 import_btn.grid(row=1,column=0)
 
 timeindex = tk.Label(root, text="測定間隔 : ")
@@ -200,7 +169,7 @@ timeshift.grid(row=2,column=2)
 time_new = tk.Entry(root)
 time_new.grid(row=2,column=3)
 
-time_override = tk.Button(root, text="更新", command = lambda:time_refresh(time_orig,time_new,range_start,range_finish,time_max,time_min))
+time_override = tk.Button(root, text="更新", command = lambda:time_refresh(time_orig,time_new))
 time_override.grid(row=2,column=4)
 
 rangeindex = tk.Label(root, text="範囲 : ")
@@ -238,3 +207,14 @@ var_index.grid(row=7,column=1)
 
 # メインループの実行
 root.mainloop()
+
+
+"""
+ファイルの中に書かれている数値以外の情報を一時的に保存しておく変数をglobalで用意
+新たにファイルを読み込むごとに変更
+どこに挟まっていたか書き留める
+
+範囲指定したものを新しいcsvファイルに書き出す
+
+error分があったらそこで切る
+"""
